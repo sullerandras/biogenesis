32fb94325ef462834bff164d08dec735315e462b
Andras Suller <suller.andras@gmail.com>
2023-08-13 20:18:12 +0700

    Set analyzer icon for Windows

--- src/biogenesis/clade_analyzer/GUI.java
+++ src/biogenesis/clade_analyzer/GUI.java
@@ -9,31 +9,39 @@ import biogenesis.Clade;
 import biogenesis.clade_analyzer.gui.MainFrame;
 
 public class GUI {
-	public static ImageIcon createIcon(String imageName) {
-		ImageIcon icon = null;
-		if (imageName != null) {
-			URL url = Clade.class.getResource(imageName);
-			if (url != null)
-				icon = new ImageIcon(url);
-		}
-		return icon;
-	}
+  public static ImageIcon createIcon(String imageName) {
+    ImageIcon icon = null;
+    if (imageName != null) {
+      URL url = Clade.class.getResource(imageName);
+      if (url != null) {
+        icon = new ImageIcon(url);
+      }
+    }
+    return icon;
+  }
 
   public static void main(String args[]) {
     // Set the name of the application menu item on macOS. Also, set the name of the application seen in Activity Monitor.
     System.setProperty("apple.awt.application.name", "Biogenesis Analyzer");
 
     // Set application icon. This should work on macOS and Windows as well.
+    ImageIcon imageIcon = createIcon("images/menu_track.png");
     try {
       final Taskbar taskbar = Taskbar.getTaskbar(); // Java 9+
-      taskbar.setIconImage(createIcon("images/menu_track.png").getImage());
-    } catch (Exception|NoClassDefFoundError e) {
+      if (imageIcon != null) {
+        taskbar.setIconImage(imageIcon.getImage());
+      }
+    } catch (Exception | NoClassDefFoundError e) {
       System.err.println("Unable to set icon: " + e);
     }
 
     java.awt.EventQueue.invokeLater(new Runnable() {
       public void run() {
-        new MainFrame().setVisible(true);
+        MainFrame mainFrame = new MainFrame();
+        if (imageIcon != null) {
+          mainFrame.setIconImage(imageIcon.getImage());
+        }
+        mainFrame.setVisible(true);
       }
     });
   }
